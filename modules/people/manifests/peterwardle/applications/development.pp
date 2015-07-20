ab2ba545e2a357454f064f5f2f905b491deaea31
Peter Wardle <peter@peterwardle.co.uk>
2015-07-20 09:47:46 +0100

    Added additional dev tools

--- modules/people/manifests/peterwardle/applications/development.pp
+++ modules/people/manifests/peterwardle/applications/development.pp
@@ -7,6 +7,9 @@ class people::peterwardle::applications::development {
         'phantomjs',
         'ngrep',
         'goaccess',
+        'watch',
+        'git-flow',
+        'wget',
     ]
 
     $applications = [
