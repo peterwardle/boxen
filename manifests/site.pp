0d35d0efb5cd6e7dd8cb51feaa3043371065062c
Peter Wardle <peter@peterwardle.co.uk>
2016-06-13 12:23:55 +0100

    remove multiple versions for ruby and node

--- manifests/site.pp
+++ manifests/site.pp
@@ -63,16 +63,16 @@ node default {
       fail('Please enable full disk encryption and try again')
   }
 
-  # node versions
-  nodejs::version { '0.8': }
-  nodejs::version { '0.10': }
-  nodejs::version { '0.12': }
+  # # node versions
+  # nodejs::version { '0.8': }
+  # nodejs::version { '0.10': }
+  # nodejs::version { '0.12': }
 
-  # default ruby versions
-  ruby::version { '1.9.3': }
-  ruby::version { '2.0.0': }
-  ruby::version { '2.1.8': }
-  ruby::version { '2.2.4': }
+  # # default ruby versions
+  # ruby::version { '1.9.3': }
+  # ruby::version { '2.0.0': }
+  # ruby::version { '2.1.8': }
+  # ruby::version { '2.2.4': }
 
   # common, useful packages
   package {
