2ccfb5df98052f05fe2f13a89bfafc333fe65377
Peter Wardle <peter@peterwardle.co.uk>
2016-07-08 17:53:59 +0100

    Updated Evaluagent provisioning

--- modules/projects/manifests/evaluagent.pp
+++ modules/projects/manifests/evaluagent.pp
@@ -10,6 +10,12 @@ class projects::evaluagent {
         provider => 'brewcask',
     }
 
+    package {
+        [
+            'kubernetes-cli'
+        ]:
+    }
+
     exec { 'download awscli':
         unless  => 'test -e /usr/local/bin/aws',
         creates => '/tmp/awscli-bundle',
@@ -22,5 +28,24 @@ class projects::evaluagent {
         cwd => '/tmp',
         command => 'sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws',
         require => Exec['download awscli'],
+    }
+
+    $scripts = ['docker', 'docker-machine', 'docker-compose']
+
+    # Puppet > 4.0
+    # $scripts.each |String $script| {
+    #     file { "/usr/local/etc/bash_completion.d/${script}.bash-completion":
+    #         ensure => "/Applications/Docker.app/Contents/Resources/etc/${script}.bash-completion",
+    #         require => Exec['docker'],
+    #     }
+    # }
+
+    define bash::completion::symlink ($src, $script = $title) {
+        file {"/usr/local/etc/bash_completion.d/${script}.bash-completion":
+            ensure => "${src}/${script}.bash-completion",
+        }
+    }
+    bash::completion::symlink { $scripts:
+        src => '/Applications/Docker.app/Contents/Resources/etc',
     }
 }
