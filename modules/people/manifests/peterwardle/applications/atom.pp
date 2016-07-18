236b99fa901b3bc08846f045f8080cad6d7b96fb
Peter Wardle <peter@peterwardle.co.uk>
2016-07-18 16:21:32 +0100

    Readded dependancy but with a check for existance before overridding bundled package

--- modules/people/manifests/peterwardle/applications/atom.pp
+++ modules/people/manifests/peterwardle/applications/atom.pp
@@ -1,13 +1,13 @@
 class people::peterwardle::applications::atom {
-    # # The autocomplete-plus package is bundled with Atom and should not be explicitly installed.
-    # exec { 'Atom:autocomplete-plus':
-    #     command => 'apm install autocomplete-plus',
-    #     require => Package['atom'],
-    # }
+    exec { 'Atom:autocomplete-plus':
+        unless => 'apm list | grep autocomplete-plus',
+        command => 'apm install autocomplete-plus',
+        require => Package['atom'],
+    }
 
     exec { 'Atom:autocomplete-php':
         unless => 'apm list | grep autocomplete-php',
         command => 'apm install autocomplete-php',
-        # require => Exec['Atom:autocomplete-plus'],
+        require => Exec['Atom:autocomplete-plus'],
     }
 }
