6e7d3f65a5523b7e987ca0acc0c07dacf99abdc9
Peter Wardle <peter@peterwardle.co.uk>
2016-07-18 15:59:41 +0100

    Added custom packages to Atom

--- modules/people/manifests/peterwardle/applications/atom.pp
+++ modules/people/manifests/peterwardle/applications/atom.pp
@@ -1,2 +1,11 @@
 class people::peterwardle::applications::atom {
+    exec { 'Install:autocomplete-plus':
+        command => 'apm install autocomplete-plus',
+        require => Package['atom'],
+    }
+
+    exec { 'Install:autocomplete-php':
+        command => 'apm install autocomplete-php',
+        require => Exec['Install:autocomplete-plus'],
+    }
 }
